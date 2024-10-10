9e7991c0d1bb140448022018b5785a31f0114929
audi putra <audiputra19@gmail.com>
2024-10-10 10:27:22 +0700

    update api url base

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -4,7 +4,8 @@ import { ApiCashRes, CashRequestReq, CashRequestRes } from "../Interfaces/main";
 
 export const api = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
+    // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://api-approval.vercel.app" }),
     endpoints: build => ({
         PinAccess: build.mutation<ApiRes<PinRes>, PinReq>({
             query: body => ({
