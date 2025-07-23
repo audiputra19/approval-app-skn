4cfb4f7f58ad6989076de5586a73a7fae7026e6c
audiputra19 <audiputra19@gmail.com>
2025-07-23 09:35:50 +0700

    update so as not to download doc file on mobile

--- src/Components/modalDoc.tsx
+++ src/Components/modalDoc.tsx
@@ -2,6 +2,7 @@ import { X } from "lucide-react";
 import { FC, useEffect } from "react";
 import { useFilePostMutation } from "../Services/api";
 import { CashFilesRes } from "../Interfaces/main";
+import Loading from "./loading";
 
 interface ModalProps {
     isOpen: boolean;
@@ -10,7 +11,7 @@ interface ModalProps {
 }
 export const ModalDoc: FC<ModalProps> = ({ isOpen, onClose, idCash }) => {
     
-    const [file, {data}] = useFilePostMutation();
+    const [file, {data, isLoading}] = useFilePostMutation();
     //console.log(data?.length)
     
     useEffect(() => {
@@ -26,10 +27,12 @@ export const ModalDoc: FC<ModalProps> = ({ isOpen, onClose, idCash }) => {
             <a 
                 href={`https://app.sknmedical.co.id/skn/fnc/cash_request/${item.file_path}`}
                 key={item.file_name}
+                target="_blank"
+                rel="noopener noreferrer"
             >
                 <span className="flex">
                     <p className="w-[25px] text-center font-bold">{index + 1}.</p>
-                    <p>{item.file_name}</p>
+                    <p className="text-blue-600 underline">{item.file_name}</p>
                 </span>
             </a>
         )
@@ -37,6 +40,7 @@ export const ModalDoc: FC<ModalProps> = ({ isOpen, onClose, idCash }) => {
 
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            {isLoading && <Loading/>}
             <div className="bg-white p-6 rounded-lg shadow-lg w-80">
                 <div className="flex justify-end cursor-pointer">
                     <X onClick={onClose}/>
